53a67455ae631716fb6db1a6b46179884ba287b5
Art Weinstein <artur.weinstein@gmail.com>
2021-08-13 11:45:49 -0700

    WIP: attempt to add themes and images to results

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,18 @@ $(document).ready(function(){
       $("#html").hide();
       $("#python").hide();
       $("#savage").hide();
-    } else if (sleepChoice === "Early Bird" || sleepChoice === "Night Owl" && transportChoice === "Motorcycle" && careerChoice === "Cyber Security" && musicChoice === "FUTURE FUNK" && gameChoice === "Gameboy Color") {
+      //I was messing with order of operations, so for HTML to show, the user has to choose either walking, web desgin and rock, OR at least funk and nintendo switch. Choosing both options at the same time also works. 
+    } else if (transportChoice === "Walking" && careerChoice === "Web Design" && musicChoice === "Rock" || musicChoice === "Funk" && gameChoice === "Nintendo Switch") {
+      $("#html").show();
+      $("#savage").hide();
+      $("#c").hide();
+      $("#python").hide();
+    } else if (sleepChoice === "Night Owl" && transportChoice === "Motorcycle" && careerChoice === "Cyber Security" && musicChoice === "FUTURE FUNK" && gameChoice === "Gameboy Color" || careerChoice === "Cyber Security" && sleepChoice === "Early Bird") {
       $("#python").show();
       $("#savage").hide();
       $("#c").hide();
       $("#html").hide();
-    }
-    
-    else{
+    } else {
       $("#savage").show();
       $("#c").hide();
       $("#html").hide();
