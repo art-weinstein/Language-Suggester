912dd60c6bd4b33b3a1367d5e513b9fdf8b037c0
Art Weinstein <artur.weinstein@gmail.com>
2021-08-13 10:40:56 -0700

    add test branches for user input

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,23 @@ $(document).ready(function(){
     const careerChoice = $("#career").val();
     const musicChoice = $("#music").val();
     const gameChoice = $("#game").val();
+    if (sleepChoice === "Early Bird" && careerChoice === "Engineer") {
+      $("#c").show();
+      $("#html").hide();
+      $("#python").hide();
+      $("#savage").hide();
+    } else if (sleepChoice === "Early Bird" || sleepChoice === "Night Owl" && transportChoice === "Motorcycle" && careerChoice === "Cyber Security" && musicChoice === "FUTURE FUNK" && gameChoice === "Gameboy Color") {
+      $("#python").show();
+      $("#savage").hide();
+      $("#c").hide();
+      $("#html").hide();
+    }
+    
+    else{
+      $("#savage").show();
+      $("#c").hide();
+      $("#html").hide();
+      $("#python").hide();
+    }
   });
 });
